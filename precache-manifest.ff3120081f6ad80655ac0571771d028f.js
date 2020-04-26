8cfc79f172a9c7e1748813219c7f44e5826f8501
cinekmazur <cinek.mazur@gmail.com>
2020-04-26 14:45:00 +0200

    Updates

---
+++ precache-manifest.ff3120081f6ad80655ac0571771d028f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf0958fd6e3854c2b41c2e8cadc014f",
+    "url": "/Cracov-Information-System-Client/index.html"
+  },
+  {
+    "revision": "6aeb5adf775bb0c204f9",
+    "url": "/Cracov-Information-System-Client/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "de407b7a44292f4f5135",
+    "url": "/Cracov-Information-System-Client/static/css/main.d487bdee.chunk.css"
+  },
+  {
+    "revision": "6aeb5adf775bb0c204f9",
+    "url": "/Cracov-Information-System-Client/static/js/2.3345b959.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/Cracov-Information-System-Client/static/js/2.3345b959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de407b7a44292f4f5135",
+    "url": "/Cracov-Information-System-Client/static/js/main.f7bcfe98.chunk.js"
+  },
+  {
+    "revision": "a0dc889f64ed8a44c212",
+    "url": "/Cracov-Information-System-Client/static/js/runtime-main.73911d6e.js"
+  },
+  {
+    "revision": "4337ef9741b4c7159b8d4d15a71ba45c",
+    "url": "/Cracov-Information-System-Client/static/media/krakow_header_1.4337ef97.jpg"
+  },
+  {
+    "revision": "e7d249f029d0ae895f96a5adf0fda49c",
+    "url": "/Cracov-Information-System-Client/static/media/krakow_header_2.e7d249f0.jpg"
+  },
+  {
+    "revision": "9d63a1018dc8ce440d0ef6fc12930b12",
+    "url": "/Cracov-Information-System-Client/static/media/krakow_header_3.9d63a101.jpg"
+  }
+]);
